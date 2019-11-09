3ce6e31f2a0bc0c536122f792076cac59d50e07d
wj510636178 <55150093+wj510636178@users.noreply.github.com>
2019-11-09 19:49:49 +0800

    helloworld add commit

--- google.c
+++ google.c
@@ -2,3 +2,32 @@
 //f(13)是0~13中包含1的有 1，10，11，12，13，所以f(13)=6
 //f(1) = 1。
 //求另一个f(n)=n的n值
+#include <stdio.h>
+int main(){
+	int i = 2,a[1000],j,count1=0,count2=1,m;
+	int b = i,x = i;
+	m = i;
+	while(1){
+		do{
+			b = b/10;
+			count1 = count1+1;
+		}while(b!=0);
+		for(j=0;j<count1;j++){
+			m = x%10;
+			x = x/10;
+			if(m==1){
+				count2 = count2+1;
+			}
+		}
+		count1 = 0;
+		if(count2==i){
+				printf("%d\n",i);
+				break;
+			}
+		i = i+1;
+		b = i;
+		x = i;
+		m = i;
+	}
+	return 0;
+} 
